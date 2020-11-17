72bd350893fd8e9255175e79f06c786fa2e41abd
web-cubr <mishaoboronov@yandex.ru>
2020-11-17 17:06:06 +0200

    about

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -10,6 +10,20 @@ var slider = {
 			}
 		});
 	},
+	
+	
+	tabs: (e) => {
+		let index = $(e.currentTarget).index();
+		
+		let owl = $(e.currentTarget).closest('section').find('.owl-carousel');
+		
+  		owl.trigger( 'to.owl.carousel', [index, 500] );
+		
+		$(e.currentTarget).closest('section').find('.js-slider-to.is-active').removeClass('is-active');
+		$(e.currentTarget).addClass('is-active');
+		
+	},
+	
 	init: () => {
 		
 		slider.apartments();
@@ -49,15 +63,44 @@ var slider = {
 			});
 		});
 		
+		$('.js-slider-about').each(function(){
+			
+			let prev = $(this).find('.owl-arrow_prev');
+			let next = $(this).find('.owl-arrow_next');
+			let parent = $(this).closest('.owl-carousel');
+			
+			$(this).owlCarousel({
+				loop: true,
+				nav: false,
+				dots: false,
+				smartSpeed: 500,
+				items: 1,
+				responsive:{
+				}
+			});
+		});
+		
 		$(document).on("click", '.owl-arrow', function() {
 
 			let dir = $(this).hasClass('owl-arrow_next') ? 'next' : 'prev';
 
-			let owl = $(this).closest('section').find('.owl-carousel')
+			let owl = $(this).closest('section').find('.owl-carousel');
 
 			owl.trigger(dir + '.owl.carousel', [500]);
 
 		});
+		
+		
+		$(document).on("click", '.js-slider-to', slider.tabs );
+		
+		
+		$('.js-slider-about').on('changed.owl.carousel', function(event) {
+			var item = event.item.index - 2;
+			$('.js-slider-to.is-active').removeClass('is-active')
+			$('.js-slider-to:nth-child(' +item+ ')').addClass('is-active');
+			
+			console.log(item);
+		})
 		
 	}
 };
